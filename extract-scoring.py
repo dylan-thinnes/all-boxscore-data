d21766782754efd43680624276c91d35f369f961
Dylan Thinnes <dylan.thinnes@protonmail.com>
2025-08-02 12:25:06 +0100

    Experimentally speed up scoring

--- extract-scoring.py
+++ extract-scoring.py
@@ -2,6 +2,7 @@
 import bs4
 import sys
 import json
+import re
 
 def convert_row(row):
     dat = {}
@@ -16,12 +17,21 @@ def convert_row(row):
 
 data = []
 for path in sys.argv[1:]:
-    #print(path)
+    print(f"Processing {path}", file=sys.stderr)
     with open(path) as fd:
-        soup = bs4.BeautifulSoup(fd, features="lxml")
-        table = soup.find('table', {'id': 'scoring'})
-        #print(table)
-        rows = table.find('tbody').find_all('tr')
-        data += [convert_row(row) for row in rows]
+        src = fd.read()
+        url = re.search('<link rel="canonical" href="(.*?)" />', src, re.DOTALL).group(1)
+        scores = [int(t) for t in re.findall('<div class="score">([0-9]+)</div>', src, re.DOTALL)]
+        table_src = re.search('<table[^>]*id="scoring".*?>.*?</table>', src, re.DOTALL).group(0)
+        soup = bs4.BeautifulSoup(table_src, features="lxml")
+        rows = soup.find('tbody').find_all('tr')
+        result = {
+            'url': url,
+            'scores': scores,
+            'rows': [convert_row(row) for row in rows]
+        }
+        #data += [result]
+        json.dump(result, sys.stdout)
+        print()
 
-json.dump(data, sys.stdout)
+#json.dump(data, sys.stdout)
