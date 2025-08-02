f146aa9f2b114703b30e30f85021df987cc80f27
Dylan Thinnes <dylan.thinnes@protonmail.com>
2025-08-02 12:59:35 +0100

    Fix newline issue

--- extract-scoring.py
+++ extract-scoring.py
@@ -20,7 +20,7 @@ for path in sys.argv[1:]:
     print(f"Processing {path}", file=sys.stderr)
     with open(path) as fd:
         src = fd.read()
-        url = re.search('<link rel="canonical" href="(.*?)" />', src, re.DOTALL).group(1)
+        url = re.search('<link rel="canonical" href="(.*?)" />', src, re.DOTALL).group(1).split('/')[-1]
         scores = [int(t) for t in re.findall('<div class="score">([0-9]+)</div>', src, re.DOTALL)]
         table_src = re.search('<table[^>]*id="scoring".*?>.*?</table>', src, re.DOTALL).group(0)
         soup = bs4.BeautifulSoup(table_src, features="lxml")
@@ -30,8 +30,7 @@ for path in sys.argv[1:]:
             'scores': scores,
             'rows': [convert_row(row) for row in rows]
         }
-        #data += [result]
-        json.dump(result, sys.stdout)
-        print()
+        outstr = json.dumps(result) # need to render to string to print out with newline in one go
+        print(outstr)
 
 #json.dump(data, sys.stdout)
